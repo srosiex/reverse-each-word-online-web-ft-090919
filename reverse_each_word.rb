35dde496f38d65eca40d5f7fee652c1461a34ff4
Sarah H <sarahrose.henderson@gmail.com>
2019-09-22 18:19:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@
 
 def reverse_each_word(sentence)
 new_sentence = []
-  sentence.split.each do |word|
+  sentence.split.collect do |word|
    new_sentence << word.reverse
 end
   new_sentence.join(" ")
