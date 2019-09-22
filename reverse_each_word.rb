f707de65cd343387a44567b46749f406d59eb854
Sarah H <sarahrose.henderson@gmail.com>
2019-09-22 18:08:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,8 +12,9 @@
 
 def reverse_each_word(sentence)
   sentence.split.each do {|word| new_sentence << word.reverse.join(' ') }
-end
+
   new_sentence
+end
 end
 
 puts reverse_each_word("This is a sentence to reverse the words of")
